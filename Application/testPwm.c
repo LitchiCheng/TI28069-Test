7a08a1e580ba87953dd03c6ddd0ef77088e5ef69
LitchiCheng <darboy@foxmail.com>
2023-03-26 16:41:05 +0800

    feat:增加死区pwm和斩波

--- Application/testPwm.c
+++ Application/testPwm.c
@@ -23,28 +23,48 @@ void pwmTestTask(void * pvParameters){
     EPwm1Regs.TBCTL.bit.HSPCLKDIV = 0;
     //增减计数模式
     EPwm1Regs.TBCTL.bit.CTRMODE = 2;
+
     #ifdef PWM1_NORMAL_TEST
     //计数器等于0，置高电平，计数值等于PRD，置低电平
     EPwm1Regs.AQCTLA.all = 0x0006;
     #endif
+
     #ifdef PWM1_PULSE_MUTABLE_TEST
     //增计数时，计数器等于CMPA，PWM1A置高电平；减计数时，计数值等于CMPA，PWM1A置低电平
     EPwm1Regs.AQCTLA.all = 0x0060;
     #endif
+
     #ifdef PWM1_A_B_COMPLEMENTARY
     //增计数时，计数器等于CMPA，PWM1A置低电平；减计数时，计数值等于CMPA，PWM1B置高电平
     EPwm1Regs.AQCTLB.all = 0x0090;
     #endif
     //1khz PWM
     EPwm1Regs.TBPRD = 90000 /2;
+
     #ifdef PWM1_NORMAL_TEST
     //%50占空比
     EPwm1Regs.CMPA.half.CMPA = 90000/4;
     #endif
+
     #ifdef PWM1_PULSE_MUTABLE_TEST
     //占空比100%
     EPwm1Regs.CMPA.half.CMPA = 0;
     #endif
+
+    #ifdef PWM_DEAD_BAND_TEST
+    //增计数时，计数器等于CMPA，PWM1A置高电平；减计数时，计数值等于CMPA，PWM1A置低电平
+    EPwm1Regs.AQCTLA.all = 0x0060;
+    //%50占空比
+    EPwm1Regs.CMPA.half.CMPA = 90000/4;
+    #endif
+
+    #ifdef PWM_CHOPPER_TEST
+    //增计数时，计数器等于CMPA，PWM1B置高电平；减计数时，计数值等于CMPA，PWM1B置低电平
+    EPwm1Regs.AQCTLB.all = 0x0600;
+    //50%占空比
+    EPwm1Regs.CMPB = 90000/4;
+    #endif
+
     #endif
     /***************************/
     #ifdef PWM2_TEST
@@ -79,6 +99,7 @@ void pwmTestTask(void * pvParameters){
     EPwm3Regs.CMPA.half.CMPA = 90000/4;
     #endif
     /***************************/
+
     #ifdef PWM_PHASE_TEST
     //当CTR为0时，同步输出
     EPwm1Regs.TBCTL.bit.SYNCOSEL = 1;
@@ -92,6 +113,21 @@ void pwmTestTask(void * pvParameters){
     EPwm3Regs.TBCTL.bit.PHSEN = 1;
     //相位240°
     EPwm3Regs.TBPHS.half.TBPHS = 90000 / 3;
+    #endif
+
+    #ifdef PWM_DEAD_BAND_TEST
+    EPwm1Regs.DBRED = 750;              // 上升沿延时10 ms
+    EPwm1Regs.DBFED = 750;              // 下降沿延时10 ms
+    EPwm1Regs.DBCTL.bit.OUT_MODE = 3;   // ePWM1A = RED
+    EPwm1Regs.DBCTL.bit.POLSEL = 2;     // S3=1 ePWM1B反相信号
+    EPwm1Regs.DBCTL.bit.IN_MODE = 0;    // ePWM1A 作为输入信号
+    #endif
+
+    #ifdef PWM_CHOPPER_TEST
+    EPwm1Regs.PCCTL.bit.CHPFREQ = 7;   // SYSCLKOUT / 64  = 2.34375MHz
+    EPwm1Regs.PCCTL.bit.CHPDUTY = 3;    // 50% 占空比
+    EPwm1Regs.PCCTL.bit.CHPEN = 1;      // 使能斩波模式
+    EPwm1Regs.PCCTL.bit.OSHTWTH = 14;   // 120 *6.67 ns = 800 ns
     #endif
 
     int updown = 1;
--- testConfig.h
+++ testConfig.h
@@ -17,6 +17,8 @@
 #define PWM2_TEST               //PWM2 输出
 #define PWM3_TEST               //PWM3 输出
 #define PWM_PHASE_TEST          //PWM 3个输出且相位差120°
+#define PWM_DEAD_BAND_TEST      //PWM1 AB死区输出，需要打开PWM1_TEST
+#define PWM_CHOPPER_TEST        //PWM1 AB互补，斩波输出，需要打开PWM1_TEST
 
 
 
